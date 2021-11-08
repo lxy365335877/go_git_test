cb30dc1cc65d15b7eae0daa5c3ae1a36025119ad
Lxy <365335877@qq.com>
2021-11-08 09:24:01 +0800

    git_go_v1

--- src/go_git_test/main.go
+++ src/go_git_test/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("这是一个demo，用于测试git")
-	fmt.Print("这是一个demo，用于测试git")
 	fmt.Print("这是一个demo1，用于测试git")
+	fmt.Print("这是一个demo2，用于测试git")
+	fmt.Print("这是一个demo3，用于测试git")
 }
